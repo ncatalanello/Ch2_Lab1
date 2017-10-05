b83d83c5c5b8f60e69d224bb6fe92df59a03088f
ncatalanello <31323057+ncatalanello@users.noreply.github.com>
2017-10-05 11:53:59 -0700

    Initial

---
+++ Names.java
@@ -0,0 +1,23 @@
+
+// ***************************************************************
+//   Names.java
+//
+//   Prints a list of student names with their hometowns
+//   and intended major
+// ***************************************************************
+public class Names
+{
+        // ------------------------
+        // main prints the list
+        // ------------------------
+        public static void main (String[] args)
+        {
+         System.out.println ();
+         System.out.println ("\tName\t\tHometown\tmajor\t");
+         System.out.println ("\t====\t\t========\t=====\t");
+         System.out.println ("\tNick\t\tPiedmont\tmath\t");
+         System.out.println ("\tAlex\t\tLafayette\tmath\t");
+         System.out.println ("\tScott\t\tLafayette\tscience\t");
+         System.out.println ();
+        }
+}
---
+++ Table.java
@@ -0,0 +1,24 @@
+// ***************************************************************
+//   Names.java
+//
+//   Prints a list of student names with their hometowns
+//   and intended major
+// ***************************************************************
+public class Table
+{
+        // ------------------------
+        // main prints the list
+        // ------------------------
+        public static void main (String[] args)
+        {
+         System.out.println ("\t--------------------------------\t"); 
+         System.out.println ("\t==\tStudent Points\t==\t");
+         System.out.println ("\t--------------------------------\t");
+         System.out.println ("\tName\t\tLab\tBonus\tTotal\t");
+         System.out.println ("\t----\t\t---\t-----\t-----\t");
+         System.out.println ("\tJoe\t\t43\t7\t50\t");
+         System.out.println ("\tWilliam\t\t50\t8\t58\t");
+         System.out.println ("\tMary Sue\t39\t10\t49\t");
+         
+        }
+}
